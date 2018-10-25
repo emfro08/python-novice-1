2ba59d8b2c2bc654facdfcb0af191e8e47013c6a
Monah <monah.aboualezz01@universitadipavia.it>
2018-10-25 14:25:12 +0200

    added .gitignore and scripts utils.py

---
+++ scripts/utilis.py
@@ -0,0 +1,8 @@
+def print_hello():
+    """This function provides me a hello"""
+    print("hello")
+    
+def print_goodbye():
+    """This function provides me a goodbye"""
+    print("goodbye")    
+    
